243de4e90d9de865e8072c36eeeba1ebb9d1bc15
khaled sidawi <khaledassa123@gmail.com>
2021-02-26 12:25:26 +0100

    done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,17 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+// Do the Fortune Teller again but this time with a object.
+
+
+function tellFortune(){
+    var numberChildren = prompt("NUMBER OF CHILDREN")
+    var partner =  prompt("PARTNER'S NAME")
+    var geographic = prompt("GEOGRAPHIC LOCATION")
+    var job = prompt("JOB TITLE")
+    window.alert( "NUMBER OF CHILDREN : " + numberChildren +"   PARTNER'S NAME : "+ partner +"   GEOGRAPHIC LOCATION :"+ geographic + "  JOB TITLE : "  + job)
+}
+
+tellFortune();
+tellFortune();
+tellFortune();
