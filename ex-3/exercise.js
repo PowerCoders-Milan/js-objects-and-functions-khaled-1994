99e8869a7cd5468b379dddf67527a25416eec937
khaled sidawi <khaledassa123@gmail.com>
2021-02-26 14:59:33 +0100

    done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,26 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+var array = ["colors", "presidents", "animal"]
+window.alert(array[0] + " :  my best color is black")
+window.alert(array[1] + " :  my best president is nothing")
+window.alert(array[2] + " :  my best animal is wolf")
+
+var colors = {
+    color : "blue " 
+}
+var presidents = {
+    thebestP : " khaled", 
+    period : "period : forever" ,
+}
+var animal = {
+    tbest : "the-best-animal : wolf" ,
+    attribiut: "attribiut : strong" ,
+}
+window.alert("my color : " + colors.color)
+window.alert("the best President is :   " +presidents.thebestP + "     and his " + presidents.period)
+
+
+window.alert(animal.tbest + "    and  " + animal.attribiut)
+
