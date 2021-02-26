bdd2f03814e66418bbed8f23c0fef9d008fdb3d7
khaled sidawi <khaledassa123@gmail.com>
2021-02-26 11:53:17 +0100

    done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+ 
+
+
+function user (name1, verb, adverb, adjective){
+    window.alert(  "NAME : " + name1 +  "  VERB : " + verb + "  ADVERB : " + adverb + " ADJECTIV : " + adjective )
+    
+ };
+ var name1 = prompt("YOUR NAME");
+ var verb = prompt("YOUR VERB");
+ var adverb = prompt("YOUR ADVERB");
+ var adjective = prompt("YOUR ADJECTIVE"); 
+ user( name1, verb, adverb, adjective);
