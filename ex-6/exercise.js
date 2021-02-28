2bb632cdb6ba3339bc621b3058a98dee970df362
khaled sidawi <khaledassa123@gmail.com>
2021-02-28 17:35:26 +0100

    done

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,19 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+function greaterNum (x,y){
+    if (x > y){
+        console.log( "The greater number  is  : " + x)
+    }
+    else if ( y > x){
+        console.log( "The greater number is  : " + y)
+    }
+    else {
+     console.log("the numbers are the same")
+    }
+    }
+    greaterNum(20,20)
+    greaterNum(12,17)
+   
